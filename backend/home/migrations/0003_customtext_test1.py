f22a4e26bb5f8b61c41d4679e9a3bb9d50fbb312
Crowdbotics <team@crowdbotics.com>
2020-06-17 22:05:18 +0000

    Update models

--- backend/home/migrations/0003_customtext_test1.py
+++ backend/home/migrations/0003_customtext_test1.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='test1',
+            model_name="customtext",
+            name="test1",
             field=models.CharField(blank=True, max_length=256, null=True),
         ),
     ]
--- backend/home/models.py
+++ backend/home/models.py
@@ -9,6 +9,7 @@ from django.db import models
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
     test1 = models.CharField(max_length=256, null=True, blank=True,)
+    test2 = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
